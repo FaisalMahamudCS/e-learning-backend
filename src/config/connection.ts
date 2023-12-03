482deeb7dda45f5c33d8ca762741d10aa9571061
Faisal Mahamud <97041463+FaisalMahamudCS@users.noreply.github.com>
2023-12-03 20:13:30 +0600

    added connection

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,7 +1,7 @@
 // src/config/database.ts
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('database', 'username', 'password', {
+export const sequelize = new Sequelize('db', 'postgresql', 'password', {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432,
