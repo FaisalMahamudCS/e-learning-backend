790d58057729a59224b41161589a974355e5f4ad
FaisalMahamudCS <fmfahimcse@gmail.com>
2024-06-17 00:48:21 +0600

    enrollment added

---
+++ src/models/Enrollment.ts
@@ -0,0 +1,81 @@
+// src/models/Course.ts
+// import { DataTypes, Model } from 'sequelize';
+// import { sequelize } from '../config/connection';
+
+// class Course extends Model {
+//   public id!: number;
+//   public name!: string;
+// }
+
+// Course.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     sequelize,
+//     modelName: 'course',
+//   }
+// );
+
+// export default Course;
+
+// models/User.ts
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+interface EnrollmentAttributes {
+  id: number;
+  courseId: string;
+  userId:string;
+  enrollmentDate:string;
+  
+  // Add other fields as needed
+}
+
+class Enrollment extends Model<EnrollmentAttributes> implements EnrollmentAttributes {
+  public id!: number;
+  public userId!: number ;
+  public courseId:number;
+  public enrollmentDate!:Date;
+  
+  // Add other fields as needed
+}
+
+export default (sequelize: Sequelize) => {
+Enrollment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        
+    },
+    courseId: {
+        type: DataTypes.INTEGER,
+       
+    },
+    enrollmentDate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+    // Add other fields as needed
+  }, {
+    sequelize,
+    modelName: 'Enrollment', // Set the model name
+    // Other options here
+  });
+
+  return Enrollment;
+};
+export {Enrollment}
+
+// export default initUserModel;
